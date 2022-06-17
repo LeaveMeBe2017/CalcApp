17815a88d329a4de9341d1501379cd37049da6de
Solar135 <lenk030@mail.ru>
2022-06-17 16:48:36 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+
+    public class Calculator
+    {
+        public double Plus(double a, double b)
+        {
+            double result = a + b;
+            return result;
+        }
+
+        public double Minus(double a, double b)
+        {
+            double result = a - b;
+            return result;
+        }
+
+        public double Multi(double a, double b)
+        {
+            double result = a * b;
+            return result;
+        }
+
+        public double Divide(double a, double b)
+        {
+            double result = a / b;
+            return result;
+        }
+
+        public double TriangleSquare(double a, double b)
+        {
+            double result = 0.5 * a * b;
+            return result;
+        }
+
+        public double RoundSquare(double a)
+        {
+            double result = Math.PI * (a * a);
+            return result;
+        }
+
+        public double Average(double[] array)
+        {
+
+            double sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum / array.Length;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
